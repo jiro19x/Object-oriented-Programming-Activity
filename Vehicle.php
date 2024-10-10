6f4e5229c9110ad6932b8afb71db8182c773d660
jiro19x <165183193+jiro19x@users.noreply.github.com>
2024-10-10 23:32:44 +0800

    Vehicle.php

--- Vehicle.php
+++ Vehicle.php
@@ -1,20 +1,25 @@
 <?php
-// Vehicle.php
 class Vehicle {
     public $name;
     public $speed;
     public $mileage;
 
+    
     public function __construct($name, $speed, $mileage) {
         $this->name = $name;
         $this->speed = $speed;
         $this->mileage = $mileage;
     }
 
+   
     public function displayInfo() {
         echo "Vehicle Name: " . $this->name . "<br>";
         echo "Speed: " . $this->speed . " km/h<br>";
         echo "Mileage: " . $this->mileage . " km<br>";
     }
 }
+
+// Testing the Vehicle class
+$vehicle = new Vehicle("AEROX V2", 200, 15000);
+$vehicle->displayInfo();
 ?>
